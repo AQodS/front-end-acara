88a060266f6c718b1f3df6675f03e56bd91c84d8
AQodS <qoddri@gmail.com>
2025-08-11 04:36:18 +0700

    fix: date standarization

--- src/components/views/Admin/DetailEvent/InfoTab/InfoTab.tsx
+++ src/components/views/Admin/DetailEvent/InfoTab/InfoTab.tsx
@@ -140,8 +140,8 @@ const InfoTab = (props: PropTypes) => {
               name="startDate"
               render={({ field }) => (
                 <DatePicker
-                  // {...field}
-                  // defaultValue={toInputDate(`${dataEvent?.startDate}`)}
+                  {...field}
+                  defaultValue={toInputDate(`${dataEvent?.startDate}`)}
                   errorMessage={errorsUpdateInfo.startDate?.message}
                   hideTimeZone
                   isInvalid={errorsUpdateInfo.startDate !== undefined}
@@ -159,8 +159,8 @@ const InfoTab = (props: PropTypes) => {
               name="endDate"
               render={({ field }) => (
                 <DatePicker
-                  // {...field}
-                  // defaultValue={toInputDate(`${dataEvent?.endDate}`)}
+                  {...field}
+                  defaultValue={toInputDate(`${dataEvent?.endDate}`)}
                   errorMessage={errorsUpdateInfo.startDate?.message}
                   hideTimeZone
                   isInvalid={errorsUpdateInfo.startDate !== undefined}
@@ -186,6 +186,7 @@ const InfoTab = (props: PropTypes) => {
                   errorMessage={errorsUpdateInfo.isPublish?.message}
                   isInvalid={errorsUpdateInfo.isPublish !== undefined}
                   label="Status"
+                  labelPlacement="outside"
                   variant="bordered"
                 >
                   <SelectItem key="true" value="true">
@@ -212,6 +213,7 @@ const InfoTab = (props: PropTypes) => {
                   errorMessage={errorsUpdateInfo.isFeatured?.message}
                   isInvalid={errorsUpdateInfo.isFeatured !== undefined}
                   label="Featured"
+                  labelPlacement="outside"
                   variant="bordered"
                 >
                   <SelectItem key="true" value="true">
